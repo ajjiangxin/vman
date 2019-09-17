eafeaf2e1613f91c34739def51e9c25f69da5800
ajjiangxin <ajjiangxin@gmail.com>
2019-09-17 12:07:19 +0800

    update

--- vman.py
+++ vman.py
@@ -54,7 +54,7 @@ class Base:
         for r in rs:
             vm, vm_info = pickle.loads(os.read(r, 4096))
             group = vm_info['group']
-            if group in self.get_groups():
+            if str(group) in self.get_groups():
                 self.info_by_groups[group][vm] = vm_info
             else:
                 self.info_by_groups['not_valid'][vm] = vm_info
@@ -190,7 +190,7 @@ class Base:
                                        key_by_value_of='name')
                 info_vm['port_forwarding'][d_key] = d
             if "Groups:" in line_raw:
-                info_vm['group'] = re.sub(r'Groups:[\s]*', '', line_raw).decode()
+                info_vm['group'] = re.sub(r'Groups:[\s]*', '', line_raw)
             if "Name" in line_raw and "Host path" in line_raw:
                 guest_dir = line_raw.split(',')[0].split(':')[1].replace('\'', '').strip()
                 host_path = line_raw.split(',')[1].split(':')[1].replace('\'', '').strip().split()[0]
