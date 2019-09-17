4acf6f758d4482be06d04a96d836c510ea5a3b2a
ajjiangxin <ajjiangxin@gmail.com>
2019-09-17 20:12:13 +0800

    update

--- template/template_provider.py
+++ template/template_provider.py
@@ -2,19 +2,19 @@
 import os
 
 # default: copy the (dummy) vagrant-node folder to current dir
-def folder_template_provider(vms):
+def folder_template_provider(vms, script_dir):
     default_folder_name = 'vagrant-node'
     vms = ['vagrant-' + name for name in vms] if vms else [default_folder_name]
     for vm in vms:
         cmd = '(rm -rf ./%s || true ) ' \
               '&& (rm -rf ./vagrant-node || true ) ' \
-              '&& cp -r template/vagrant-node ./%s && mv ./%s/vmconfig_template.yml ./%s/vmconfig.yml'
-        os.popen(cmd % (vm, vm, vm, vm))
+              '&& cp -r %s/template/vagrant-node ./%s && mv ./%s/vmconfig_template.yml ./%s/vmconfig.yml'
+        os.popen(cmd % (vm, script_dir, vm, vm, vm))
 
 
 
 # clone a git repo to current path, which holds your vagrant template and scripts
-def git_repo_template_provider(vms):
+def git_repo_template_provider(vms, script_dir):
     default_folder_name = 'vagrant-node'
     vms = ['vagrant-' + name for name in vms] if vms else [default_folder_name]
     for vm in vms:
