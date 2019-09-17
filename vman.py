5e21c4c92ff2baaeaaa7a8b42e4dbbaefbe2d167
ajjiangxin <ajjiangxin@gmail.com>
2019-09-17 11:28:20 +0800

    update

--- vman.py
+++ vman.py
@@ -230,7 +230,7 @@ class GroupCMD(Base):
         self.group = '/' + self.args[0]
         if self.group in self.get_groups():
             print("%s:" % self.group)
-            for vm, info in self.get_info_by_groups()[self.group].items():
+            for vm, info in self.get_info_by_groups()[self.group]:
                 print(vm)
                 # self.print_vm_info(2, vm, info)
             print("\n")
