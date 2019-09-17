72a4681ae77d2a4339b34d62a3bbd1aa853dbcb4
ajjiangxin <ajjiangxin@gmail.com>
2019-09-17 20:10:34 +0800

    update

--- template/template_provider.py
+++ template/template_provider.py
@@ -6,8 +6,10 @@ def folder_template_provider(vms):
     default_folder_name = 'vagrant-node'
     vms = ['vagrant-' + name for name in vms] if vms else [default_folder_name]
     for vm in vms:
-        cmd = '(rm -rf %s || true ) && cp -r %s ./%s && mv ./%s/vmconfig_template.yml ./%s/vmconfig.yml'
-        os.popen(cmd % ('./' + vm, default_folder_name + '/' + default_folder_name, vm, vm, vm))
+        cmd = '(rm -rf ./%s || true ) ' \
+              '&& (rm -rf ./vagrant-node || true ) ' \
+              '&& cp -r template/vagrant-node ./%s && mv ./%s/vmconfig_template.yml ./%s/vmconfig.yml'
+        os.popen(cmd % (vm, vm, vm, vm))
 
 
 
@@ -16,11 +18,10 @@ def git_repo_template_provider(vms):
     default_folder_name = 'vagrant-node'
     vms = ['vagrant-' + name for name in vms] if vms else [default_folder_name]
     for vm in vms:
-        cmd = 'git clone git@gitlab.aj:ajjiangxin/node.git %s && cp %s/properties_template.yml %s/properties.yml'
+        cmd = 'git clone git@gitlab.aj:ajjiangxin/node.git %s && cp ./%s/properties_template.yml ./%s/properties.yml'
         os.popen(cmd % (vm, vm, vm))
-
-
+    return
 
 # a method to init vagrantfile on current path (where vman executes)
 default_provider = folder_template_provider
-init_vagrant_vms = git_repo_template_provider
+init_vagrant_vms = default_provider
